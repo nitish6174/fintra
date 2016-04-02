e29b9b8dd00dd4649c521649782846c137c564b7
nitish6174 <nitish.garg.6174@gmail.com>
2016-04-03 04:50:39 +0530

    Added recent search results

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,33 @@
-$('#result').html('Results will be displayed here');
+$('#result').html('<h3 class="text-center">Search results will be displayed here</h3>');
 
 $('#search').click( function() {
+	search($('#query').val(),$('#mode').val());
+});
+
+function search(query,mode)
+{
+	$('#recent_list').append('<div class="history_item" onclick="search(\''+query+'\',\''+mode+'\')">'+'('+mode+') '+query+'</div>');
 	$.ajax({
 		url: '',
 		type: 'POST',
 		data: {
-			query: $('#query').val(),
-			mode: $('#mode').val()
+			query: query,
+			mode: mode
 		},
 		success: function(data) {
-			var links = (JSON.parse(data))['links'];
+			var links = (JSON.parse(data))['links'];			
 			$('#result').html('');
 			if(links.length>0)
 			{
 				for(i=0;i<links.length;i++)
 				{
-					$('#result').append('<div><a href="'+links[i]+'">'+links[i]+'</a></div>');
+					$('#result').append('<div><a href="'+links[i]+'" target="_blank">'+links[i]+'</a></div>');
 				}
 			}
 			else
 			{
-				$('#result').html('No results found for this query.');
+				$('#result').html('<h4 class="text-center">No results found for this query</h4>');
 			}
 		}
 	});
-});
+}
